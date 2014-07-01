b5e037393a69e0c8dadbe36de0a095ae8ce74dbc
Ryan Uber <ru@ryanuber.com>
2014-06-30 17:36:07 -0700

    Added initial license tests

---
+++ license_test.go
@@ -0,0 +1,55 @@
+package license
+
+import (
+	"io/ioutil"
+	"testing"
+)
+
+func TestNewLicense(t *testing.T) {
+	l := New("MyLicense", "Some license text.")
+	if l.Type != "MyLicense" {
+		t.Fatalf("bad license type: %s", l.Type)
+	}
+	if l.Text != "Some license text." {
+		t.Fatalf("bad license text: %s", l.Text)
+	}
+}
+
+func TestNewFromFile(t *testing.T) {
+	f, err := ioutil.TempFile("", "go-license")
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+	defer f.Close()
+
+	f.WriteString(`
+The MIT License (MIT)
+
+Copyright (c) <year> <copyright holders>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+`)
+
+	l, err := NewFromFile(f.Name())
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	l.Recognized()
+}
