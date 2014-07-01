8bdf442b330b3d40c66122613de9aba0549ed558
Ryan Uber <ru@ryanuber.com>
2014-06-30 17:51:04 -0700

    Added tests for reading a license from a file or directory

--- license_test.go
+++ license_test.go
@@ -2,6 +2,8 @@ package license
 
 import (
 	"io/ioutil"
+	"os"
+	"path/filepath"
 	"testing"
 )
 
@@ -20,37 +22,48 @@ func TestNewFromFile(t *testing.T) {
 	if err != nil {
 		t.Fatalf("err: %s", err)
 	}
-	defer f.Close()
+	defer os.Remove(f.Name())
 
-	licenseText := `
-The MIT License (MIT)
-
-Copyright (c) <year> <copyright holders>
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-`
+	licenseText := "The MIT License (MIT)"
 
 	if _, err := f.WriteString(licenseText); err != nil {
 		t.Fatalf("err: %s", err)
 	}
 
 	l, err := NewFromFile(f.Name())
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	if l.Type != "MIT" {
+		t.Fatalf("unexpected license type: %s", l.Type)
+	}
+
+	if l.Text != licenseText {
+		t.Fatalf("unexpected license text: %s", l.Text)
+	}
+}
+
+func TestNewFromDir(t *testing.T) {
+	d, err := ioutil.TempDir("", "go-license")
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+	defer os.RemoveAll(d)
+
+	fPath := filepath.Join(d, "LICENSE")
+	f, err := os.Create(fPath)
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	licenseText := "The MIT License (MIT)"
+
+	if _, err := f.WriteString(licenseText); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	l, err := NewFromDir(d)
 	if err != nil {
 		t.Fatalf("err: %s", err)
 	}
